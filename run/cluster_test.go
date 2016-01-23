1294f13319a83923b57db9527cf901543ff576ae
Gyu-Ho Lee <gyuhox@gmail.com>
2016-01-22 22:57:59 -0800

    run: add cluster test

---
+++ run/cluster_test.go
@@ -0,0 +1,37 @@
+package run
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/satori/go.uuid"
+)
+
+func TestCreateCluster(t *testing.T) {
+	isClientTLS := false
+	isPeerTLS := false
+	certPath := ""
+	privateKeyPath := ""
+	caPath := ""
+	fs := make([]*Flags, 3)
+	for i := range fs {
+		df, err := NewFlags(fmt.Sprintf("etcd%d", i), nil, 11+i, "etcd-cluster-token", "new", uuid.NewV4().String(), isClientTLS, isPeerTLS, certPath, privateKeyPath, caPath)
+		if err != nil {
+			t.Error(err)
+		}
+		fs[i] = df
+	}
+	CombineFlags(fs...)
+	c, err := CreateCluster(os.Stdout, "bin/etcd", fs...)
+	if err != nil {
+		t.Error(err)
+	}
+	fmt.Println("c.WriteProc()")
+	c.WriteProc(os.Stdout)
+
+	fmt.Println()
+	for k, v := range c.NameToMember {
+		fmt.Printf("%s's ListenClientURLs: %v\n", k, v.Flags.ListenClientURLs)
+	}
+}
