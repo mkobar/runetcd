ed6d224021ce5ba603f988ad4ada7f848d8dda50
Gyu-Ho Lee <gyuhox@gmail.com>
2016-01-23 01:26:05 -0800

    main.go add more flags

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"time"
 
 	log "github.com/Sirupsen/logrus"
+	"github.com/gyuho/psn/ss"
+	"github.com/spf13/cobra"
 )
 
 func init() {
@@ -33,4 +35,66 @@ type GlobalFlag struct {
 	DemoConnectionNumber int
 	DemoClientNumber     int
 	DemoStressNumber     int
+
+	DemoIsClientTLS    bool
+	DemoIsPeerTLS      bool
+	DemoCertPath       string
+	DemoPrivateKeyPath string
+	DemoCAPath         string
+
+	// demo-web command
+	DemoWebSimple           bool
+	DemoWebPause            time.Duration
+	DemoWebConnectionNumber int
+	DemoWebClientNumber     int
+	DemoWebStressNumber     int
+}
+
+var (
+	// need a way to clean this
+	globalFlag  = GlobalFlag{}
+	globalPorts = ss.NewPorts()
+
+	nameToTerminate = "etcd1"
+	nameToStress    = "etcd2"
+
+	stressKeyN = 5
+	stressValN = 5
+
+	certPath       = "testcerts/cert.pem" // signed key-pair
+	privateKeyPath = "testcerts/key.pem"  // signed key-pair
+	caPath         = "testcerts/ca.perm"  // CA certificate
+
+	rootCommand = &cobra.Command{
+		Use:        cliName,
+		Short:      cliDescription,
+		SuggestFor: []string{"runetfd", "rnetcd", "runetdc"},
+	}
+	demoCommand = &cobra.Command{
+		Use:   "demo",
+		Short: "demo demos etcd in terminal.",
+		Run:   demoCommandFunc,
+	}
+	demoWebCommand = &cobra.Command{
+		Use:   "demo-web",
+		Short: "demo-web demos etcd in a web browser.",
+		Run:   demoWebCommandFunc,
+	}
+	killCommand = &cobra.Command{
+		Use:   "kill",
+		Short: "kill kills etcd.",
+		Run:   killCommandFunc,
+	}
+)
+
+func init() {
+	globalPorts.Refresh()
+	go func() {
+		for {
+			select {
+			case <-time.After(globalFlag.IntervalPortRefresh):
+				globalPorts.Refresh()
+			}
+		}
+	}()
 }
