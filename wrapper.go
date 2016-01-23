f63af3a2b1e6cf94d9ab230f9d4e05c564868f94
Gyu-Ho Lee <gyuhox@gmail.com>
2016-01-23 02:25:44 -0800

    add cache wrapper

--- wrapper.go
+++ wrapper.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	log "github.com/Sirupsen/logrus"
+	"github.com/gorilla/websocket"
 	"golang.org/x/net/context"
 )
 
@@ -32,4 +33,38 @@ func (ca *ContextAdapter) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 			"error":      err,
 		}).Errorln("ServeHTTP error")
 	}
+}
+
+func withUserCache(h ContextHandler) ContextHandler {
+	return ContextHandlerFunc(func(ctx context.Context, w http.ResponseWriter, req *http.Request) error {
+
+		userID := getUserID(req)
+		ctx = context.WithValue(ctx, userKey, &userID)
+
+		globalCache.mu.Lock()
+		// (X) this will deadlock
+		// defer globalCache.mu.Unlock()
+		if globalCache.perUserID == nil {
+			globalCache.perUserID = make(map[string]*userData)
+		}
+		if _, ok := globalCache.perUserID[userID]; !ok {
+			globalCache.perUserID[userID] = &userData{
+				upgrader: &websocket.Upgrader{},
+				cluster:  nil,
+				donec:    make(chan struct{}),
+
+				bufStream: make(chan string, 5000),
+
+				ctlHistory: []string{
+					`etcdctlv3 put YOUR_KEY YOUR_VALUE`,
+					`etcdctlv3 range YOUR_KEY`,
+				},
+			}
+		}
+		globalCache.mu.Unlock()
+
+		// TODO: reset this periodically
+
+		return h.ServeHTTPContext(ctx, w, req)
+	})
 }
